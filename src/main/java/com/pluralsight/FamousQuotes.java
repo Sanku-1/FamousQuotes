4c8426db52aeb2c897e904d6589a6a8780f5168c
Stephen_A <ankustephen99@gmail.com>
2025-10-06 11:12:58 -0400

    added try/catch error handling

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 
 public class FamousQuotes {
     public static void main(String[] args) {
+        try {
         String[] quoteList = {"Time and tide wait for no man", "What goes around comes around" , "An eye for a eye makes everyone blind", "An apple a day keeps the doctor away", "The early bird catches the worm", "Sticks and stones may break my bones, but words will never hurt me", "We're all friends in the clurb", "The unexamined life is not worth living" , "The best revenge is massive success", "You miss 100 percent of the shots you don't take"};
         System.out.println(quoteList[0]);
         Scanner quoteScanner = new Scanner(System.in);
         System.out.println("Please select which quote you would like to read: (1-10)");
         int quouteSelection = quoteScanner.nextInt();
         System.out.println(quoteList[quouteSelection - 1]);
+        }
+        catch(Exception e) {
+            System.out.println("Invalid Quote Selection");
+        }
     }
 }
