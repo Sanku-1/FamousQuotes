610c83097e19729fbc5f032d73dc651bd7d7d1d8
Stephen_A <ankustephen99@gmail.com>
2025-10-06 11:36:48 -0400

    started implementation of switch case

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -5,15 +5,32 @@ import java.util.Scanner;
 public class FamousQuotes {
     public static void main(String[] args) {
         try {
-        String[] quoteList = {"Time and tide wait for no man", "What goes around comes around" , "An eye for a eye makes everyone blind", "An apple a day keeps the doctor away", "The early bird catches the worm", "Sticks and stones may break my bones, but words will never hurt me", "We're all friends in the clurb", "The unexamined life is not worth living" , "The best revenge is massive success", "You miss 100 percent of the shots you don't take"};
-        System.out.println(quoteList[0]);
-        Scanner quoteScanner = new Scanner(System.in);
-        System.out.println("Please select which quote you would like to read: (1-10)");
-        int quouteSelection = quoteScanner.nextInt();
-        System.out.println(quoteList[quouteSelection - 1]);
-        }
-        catch(Exception e) {
-            System.out.println("Invalid Quote Selection");
+            String[] quoteList = {"Time and tide wait for no man", "What goes around comes around", "An eye for a eye makes everyone blind", "An apple a day keeps the doctor away", "The early bird catches the worm", "Sticks and stones may break my bones, but words will never hurt me", "We're all friends in the clurb", "The unexamined life is not worth living", "The best revenge is massive success", "You miss 100 percent of the shots you don't take"};
+            boolean isDone = false;
+            Scanner quoteScanner = new Scanner(System.in);
+            while (!isDone) {
+                System.out.println("Please select which quote you would like to read: (1-10)");
+                int quoteSelection = quoteScanner.nextInt();
+                String repeatStep = "no";
+                switch (quoteSelection) {
+                    case 1:
+                        System.out.println(quoteList[quoteSelection - 1]);
+                        quoteScanner.nextLine();
+                        System.out.println("Would you like to read another quote? (yes/no)");
+                        repeatStep = quoteScanner.nextLine();
+                        if (repeatStep.equalsIgnoreCase("yes")) {
+                            System.out.println("Please select which quote you would like to read: (1-10)");
+                        } else {
+                            isDone = true;
+                            break;
+                        }
+                    case 2:
+                        break;
+
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid Selection");
         }
     }
 }
