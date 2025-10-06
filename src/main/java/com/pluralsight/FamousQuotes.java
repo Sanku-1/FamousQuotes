589689b784a463af75c328440d6c053a6592af14
Stephen_A <ankustephen99@gmail.com>
2025-10-06 11:05:16 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class FamousQuotes {
+    public static void main(String[] args) {
+        String[] quoteList = {"Time and tide wait for no man", "What goes around comes around" , "An eye for a eye makes everyone blind", "An apple a day keeps the doctor away", "The early bird catches the worm", "Sticks and stones may break my bones, but words will never hurt me", "We're all friends in the clurb", "The unexamined life is not worth living" , "The best revenge is massive success", "You miss 100 percent of the shots you don't take"};
+        System.out.println(quoteList[0]);
+    }
+}
